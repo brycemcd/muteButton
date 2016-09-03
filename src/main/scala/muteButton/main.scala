9955dd56347fb21303c529192ee4e22a1d07d73c
Bryce McDonnell <bryce@bridgetownint.com>
2016-09-03 13:04:24 -0400

    predicing new data stubbed out

--- src/main/scala/muteButton/main.scala
+++ src/main/scala/muteButton/main.scala
@@ -18,6 +18,11 @@ import org.apache.log4j.Level
 
 object Main {
 
+  def main(args: Array[String]) = {
+    Logger.getRootLogger().setLevel(Level.ERROR)
+    predictFromModel()
+  }
+
   def mainStream(args: Array[String]) = {
     //val conf = new SparkConf().setMaster("local[2]").setAppName("muteButton")
     ////val sc = new SparkContext(conf)
@@ -37,36 +42,41 @@ object Main {
     //ssc.awaitTermination()  // Wait for the computation to terminate
   }
 
-  def main(args: Array[String]) = {
-    val conf = new SparkConf().setMaster("local[2]").setAppName("muteButton")
-    val sc = new SparkContext(conf)
-    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-    Logger.getRootLogger().setLevel(Level.ERROR)
+
+  lazy val conf = new SparkConf().setMaster("local[2]").setAppName("muteButton")
+  lazy val sc = new SparkContext(conf)
+  def trainModel() = {
     // Loads data.
     val lines = sc.textFile("/media/brycemcd/filestore/spark2bkp/football/freqs-60s.txt")
     val meanByKey = FrequencyIntensity.convertFileContentsToMeanIntensities(lines)
 
-    println("====")
-    meanByKey.map( _._2 ).take(16).map(println)
-    println("====")
-    //val kmeans = new KMeans().setK(2).setSeed(1L).setFeaturesCol("features")
-    //import sqlContext.implicits._
-
-    //case class Freq(freq: String, inten: Double)
-    //val df = meanByKey.map( x => Freq(x._1.toString(), x._2) ).toDF()
-    //df.registerTempTable("df")
-    //val model = kmeans.fit(df)
-    //
-    // take 2
+    // Trains a k-means model.
     val numClusters = 2
     val numIterations = 20
     val parsedData = meanByKey.map( freq => Vectors.dense( freq._2 ) )
     val model = KMeans.train(parsedData, numClusters, numIterations)
 
+    val modelSaveString = "models/kmeans.model-" + System.currentTimeMillis()
+    model.save(sc, modelSaveString)
+
     println(model.toPMML())
     println("====")
 
-    // Trains a k-means model.
+    sc.stop()
+  }
+
+  def predictFromModel() = {
+    val modelPath = "models/kmeans.model-1472921561028"
+    val model = KMeansModel.load(sc, modelPath)
+
+    val lines = sc.textFile("/media/brycemcd/filestore/spark2bkp/football/freqs-5s.txt")
+    val meanByKey = FrequencyIntensity.convertFileContentsToMeanIntensities(lines)
+    meanByKey.foreach { mbk =>
+      val vec = Vectors.dense( mbk._2 )
+      println("prediction: ----")
+      println( model.predict(vec) )
+      println("----")
+    }
 
     sc.stop()
   }
