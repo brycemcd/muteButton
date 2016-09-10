660c30ed11ed68400303aee575d6fa2a4cee0f46
Bryce McDonnell <bryce@bridgetownint.com>
2016-09-10 15:08:24 -0400

    Creates a script to split training frequencies, label them and recombine

--- extra/split_training_freqs.sh
+++ extra/split_training_freqs.sh
@@ -1,16 +1,43 @@
 #!/bin/bash
 
-# split file
-split ad_and_game.txt -l 2048 freq
+# This script converts freq/intens text files into labeled samples with the format
+# freq intensity label
 
-# add filename as a column
-for f in freq*
-do
-  sed -i "s:$:  $f:" $f
+basedir=/media/brycemcd/filestore/spark2bkp/football/supervised_samples
+catArr=(ad game)
+
+returnDir=$(pwd)
+
+for c in ${catArr[@]}; do
+  cd $basedir/$c/freqs
+  gameFiles=$(ls *freqs.txt)
+
+  echo "labeling text files in $c"
+
+  for f in $gameFiles; do
+    filename=$(basename "$f")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+
+    # split file
+    split ${filename}.${extension} -l 2048 $filename
+
+    # add filename as a column
+    # NOTE: This directory will contain raw txt files that should NOT be labeled
+    # by this script
+    for labeledFile in $(ls *freqs[a-z]*)
+    do
+      sed -i "s:$:  $labeledFile:" $labeledFile
+    done
+
+    # recombine
+    cat *freqs[a-z]* > $filename-labeled.txt
+
+    # remove artifacts
+    rm *freqs[a-z]*
+
+    echo "labeled $filename"
+
+  done
 done
-
-# recombine
-cat freq* > nyg_mia_20151214-labeled.txt
-
-# remove artifacts
-rm freq*
+cd $returnDir
