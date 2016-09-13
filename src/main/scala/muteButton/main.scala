97d977cfdb72bca8106cdbaf27ea7fe28d2b0d41
Bryce McDonnell <bryce@bridgetownint.com>
2016-09-13 19:59:21 -0400

    checkpoint - transition to ml

--- src/main/scala/muteButton/main.scala
+++ src/main/scala/muteButton/main.scala
@@ -8,21 +8,28 @@ import org.apache.spark.streaming._
 import org.apache.spark.streaming.dstream.ReceiverInputDStream
 import org.apache.spark.streaming.dstream.DStream
 
-import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
+//import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
 import org.apache.spark.mllib.linalg.Vectors
 
-import org.apache.spark.mllib.classification.{LogisticRegressionWithSGD,LogisticRegressionWithLBFGS, LogisticRegressionModel}
-import org.apache.spark.mllib.evaluation.MulticlassMetrics
+//import org.apache.spark.mllib.classification.{LogisticRegressionWithSGD,LogisticRegressionWithLBFGS, LogisticRegressionModel}
+//import org.apache.spark.mllib.evaluation.MulticlassMetrics
 import org.apache.spark.mllib.regression.LabeledPoint
 
-import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.spark.ml.classification.{BinaryLogisticRegressionSummary, LogisticRegression}
 
 import org.apache.spark.sql.SQLContext
-import sqlContext.implicits._
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+//import sqlContext.implicits._
 
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
+
+// spark.ml
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.classification.{BinaryLogisticRegressionSummary, LogisticRegression}
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types.{StructType,StructField,StringType};
+
 
 // NOTE: I just lifted this out of ml-playground. Remove it and just use
 // the playground later. I really should be using it here but I'm on a roll
@@ -63,7 +70,7 @@ object Main {
     // Loads data.
     // NOTE that freq is a somewhat "magic" (now conventional ;) ) prepend string
     // for training prepared data
-    val trainAdLines = sc.textFile("/media/brycemcd/filestore/spark2bkp/football/supervised_samples/ad/freqs/*-labeled.txt")
+    val trainAdLines = sc.textFile("/media/brycemcd/filestore/spark2bkp/football/supervised_samples/ad/freqs/ari_phi_chunked094_freqs-labeled.txt")
     val trainGameLines = sc.textFile("/media/brycemcd/filestore/spark2bkp/football/supervised_samples/game/freqs/*-labeled.txt")
 
     //println("training ad lines " + trainAdLines.count())
@@ -76,26 +83,37 @@ object Main {
     //println("training game touples " + trainGameTouples.count())
 
 
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
     // NOTE: I should start with a collection of (filename, (freq, intensity)) tuples
     // NOTE: There's a slight risk that frequencies and intensity examples will
     // get mixed up when this is distributed across a cluster
+    val schemaRecord = trainAdTouples.groupByKey().map(_._2).map(_.toArray.sortBy(_._1)).first()
+    val fields = schemaRecord.map { case(freq, inten) => StructField(freq.toString, DoubleType, false) }
+    val schema = StructType( StructField("label", DoubleType, false) +: fields )
+
     val trainAdPoints = trainAdTouples.groupByKey().map(_._2).map(_.toArray.sortBy(_._1)).map { x =>
-      new LabeledPoint(1.0, Vectors.dense( x.map(_._2) ))
+      val duh = ("label", 1.0) +: x
+      Row.fromSeq( duh.map(_._2) )
     }
 
-    val trainGamePoints = trainGameTouples.groupByKey().map(_._2).map(_.toArray.sortBy(_._1)).map { x =>
-      new LabeledPoint(0.0, Vectors.dense( x.map(_._2) ))
-    }
+    val adDataFrame = sqlContext.createDataFrame(trainAdPoints, schema)
+
+    trainAdPoints.take(5).foreach(x => println(x(0)))
+
+    //val trainGamePoints = trainGameTouples.groupByKey().map(_._2).map(_.toArray.sortBy(_._1)).map { x =>
+      //new LabeledPoint(0.0, Vectors.dense( x.map(_._2) ))
+    //}.take(10)
 
 
     //println("training ad points " + trainAdPoints.count())
     //println("training game points " + trainGamePoints.count())
 
-    val allPoints = trainGamePoints.union(trainAdPoints).toDF()
+    //val allPoints = trainGamePoints.union(trainAdPoints)
 
-    val splits = allPoints.randomSplit(Array(0.8, 0.2), seed = 11L)
-    val training = splits(0).cache()
-    val test = splits(1).cache()
+    //val splits = allPoints.randomSplit(Array(0.8, 0.2), seed = 11L)
+    val training = adDataFrame
+    //val training = splits(0).cache()
+    //val test = splits(1).cache()
 
     // header:
     println("regParam, iterations, f1, precision, recall")
@@ -105,6 +123,8 @@ object Main {
         //setNumIterations(modelParam.numIterations).
         //setRegParam(modelParam.regParam)
       //val model = fit.run(training)
+
+        // TODO: this is the new one
         val fit = new LogisticRegression()
           .setMaxIter(modelParam.numIterations)
           .setRegParam(modelParam.regParam)
@@ -112,19 +132,19 @@ object Main {
         val model = fit.fit(training)
 
 
-      //model.clearThreshold()
+      ////model.clearThreshold()
       val trainingSummary = model.summary
       val objectiveHistory = trainingSummary.objectiveHistory
       objectiveHistory.foreach(loss => println(loss))
 
 
-      val predictionAndLabels = test.map { case LabeledPoint(label, features) =>
-        val prediction = model.predict(features)
-        (prediction, label)
-      }
+      //val predictionAndLabels = test.map { case LabeledPoint(label, features) =>
+        //val prediction = model.predict(features)
+        //(prediction, label)
+      //}
 
-      val metrics = new MulticlassMetrics(predictionAndLabels)
-      println(modelParam.regParam + "," + modelParam.numIterations + "," + metrics.fMeasure + "," + metrics.precision + "," + metrics.recall)
+      //val metrics = new MulticlassMetrics(predictionAndLabels)
+      //println(modelParam.regParam + "," + modelParam.numIterations + "," + metrics.fMeasure + "," + metrics.precision + "," + metrics.recall)
     }
 
     //println("training all points " + allPoints.count())
@@ -154,28 +174,28 @@ object Main {
 
   def predictFromStream(negativeAction : () => Int,
                         positiveAction : () => Int) = {
-    val modelPath = "models/kmeans.model-1473003588743"
-    val model = KMeansModel.load(sc, modelPath)
+    //val modelPath = "models/kmeans.model-1473003588743"
+    //val model = KMeansModel.load(sc, modelPath)
 
-    val lines = ssc.socketTextStream("10.1.2.230", 9999)
-    Logger.getRootLogger().setLevel(Level.ERROR)
+    //val lines = ssc.socketTextStream("10.1.2.230", 9999)
+    //Logger.getRootLogger().setLevel(Level.ERROR)
 
-    val meanByKey = FrequencyIntensityStream.convertFileContentsToMeanIntensities(lines)
-    meanByKey.foreachRDD { mbk =>
-      val vec = Vectors.dense( mbk.map(_._2).take( frequenciesInWindow ) )
-      if(vec.size == frequenciesInWindow) {
-        val prediction = model.predict(vec)
-        println("prediction: ---- " + prediction + " ----")
-        if(prediction == 0) negativeAction() else positiveAction()
-      } else {
-        print("not 2048: ")
-        print(vec.size)
-      }
-      println()
-    }
+    //val meanByKey = FrequencyIntensityStream.convertFileContentsToMeanIntensities(lines)
+    //meanByKey.foreachRDD { mbk =>
+      //val vec = Vectors.dense( mbk.map(_._2).take( frequenciesInWindow ) )
+      //if(vec.size == frequenciesInWindow) {
+        //val prediction = model.predict(vec)
+        //println("prediction: ---- " + prediction + " ----")
+        //if(prediction == 0) negativeAction() else positiveAction()
+      //} else {
+        //print("not 2048: ")
+        //print(vec.size)
+      //}
+      //println()
+    //}
 
-    ssc.start()
-    ssc.awaitTermination()  // Wait for the computation to terminate
-    sc.stop()
+    //ssc.start()
+    //ssc.awaitTermination()  // Wait for the computation to terminate
+    //sc.stop()
   }
 }
